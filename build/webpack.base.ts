e5efa728582db1bb73e42cf2c183dc57baae9771
mingK <zhengmingkun1996@sina.com>
2023-02-27 18:01:16 +0800

    feat: process.env

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -1,7 +1,14 @@
-import { Configuration } from 'webpack';
+import { Configuration, DefinePlugin } from 'webpack';
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import {BundleAnalyzerPlugin}  from 'webpack-bundle-analyzer'
 import path from "path";
+
+import * as dotenv from "dotenv";
+
+// 加载配置文件
+const envConfig = dotenv.config({
+  path: path.resolve(__dirname, "../env/.env." + process.env.BASE_ENV),
+});
+
 
 const baseConfig: Configuration = {
   entry: path.join(__dirname, "../src/index.tsx"), // 入口文件
@@ -26,19 +33,37 @@ const baseConfig: Configuration = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
+    // 别名需要配置两个地方，这里和 tsconfig.json
+    alias: {
+      "@": path.join(__dirname, "../src")
+    },
+    modules: [path.resolve(__dirname, "../node_modules")], // 查找第三方模块只在本项目的node_modules中查找
   },
   // plugins
   plugins: [
     new HtmlWebpackPlugin({
+      title: "webpack5-react-ts",
+      filename: "index.html",
       // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
       template: path.join(__dirname, "../public/index.html"),
+      inject: true, // 自动注入静态资源
+      hash: true,
+      cache: false,
       // 压缩html资源
       minify: {
+        removeAttributeQuotes: true,
         collapseWhitespace: true, //去空格
         removeComments: true, // 去注释
+        minifyJS: true, // 在脚本元素和事件属性中缩小JavaScript(使用UglifyJS)
+        minifyCSS: true, // 缩小CSS样式元素和样式属性
       },
+      nodeModules: path.resolve(__dirname, "../node_modules"),
     }),
-    new BundleAnalyzerPlugin()
+    new DefinePlugin({
+      "process.env": JSON.stringify(envConfig.parsed),
+      "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
 };
 
--- build/webpack.dev.ts
+++ build/webpack.dev.ts
@@ -1,33 +1,51 @@
 import path from "path";
 import { merge } from "webpack-merge";
-import { Configuration as WebpackConfiguration } from "webpack";
+import webpack, { Configuration as WebpackConfiguration } from "webpack";
+import WebpackDevServer from "webpack-dev-server";
 import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 import baseConfig from "./webpack.base";
+
+// 运行命令的时候重启一次打开一个tab 页很烦，所以呢优化一下
+// 参考：create-react-app 的启动方式
+// https://github.com/facebook/create-react-app/blob/main/packages/react-dev-utils/openChrome.applescript
+// 记得关闭webpack-dev-server的配置中的自动打开 open: false 或者注释
+const openBrowser = require("./util/openBrowser");
+
 
 interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
 const host = "127.0.0.1";
-const port = "8080";
+const port = "8082";
 
 // 合并公共配置,并添加开发环境配置
 const devConfig: Configuration = merge(baseConfig, {
   mode: "development", // 开发模式,打包更加快速,省了代码优化步骤
   devtool: "eval-cheap-module-source-map",
-  devServer: {
-    host,
-    port,
-    open: true, // 是否自动打开
-    compress: false, // gzip压缩,开发环境不开启，提升热更新速度
-    hot: true, // 开启热更新
-    historyApiFallback: true, // 解决history路由404问题
+});
+
+const devServer = new WebpackDevServer(
+  {
+    host, // 地址
+    port, // 端口
+    open: false, // 是否自动打开，关闭
     setupExitSignals: true, // 允许在 SIGINT 和 SIGTERM 信号时关闭开发服务器和退出进程。
+    compress: false, // gzip压缩,开发环境不开启,提升热更新速度
+    hot: true, // 开启热更新，后面会讲react模块热替换具体配置
+    historyApiFallback: true, // 解决history路由404问题
     static: {
       directory: path.join(__dirname, "../public"), // 托管静态资源public文件夹
     },
-    // headers: { "Access-Control-Allow-Origin": "*" },
+    headers: { "Access-Control-Allow-Origin": "*" },
   },
+  webpack(devConfig)
+);
+
+devServer.start().then(() => {
+  // 启动界面
+  openBrowser(`http://${host}:${port}`);
 });
+
 
 export default devConfig;
