b4ef372a922fdb229dc5abfce15badb07bb2664f
xiaoming <2885546627@qq.com>
2023-03-13 20:24:35 +0800

    feat: add code validate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styles from './App.less'
 
 function App() {
-  return <h2 className={styles.h2}><h1>dasdad</h1>Hellosdddsdsasdad Xiaoming</h2>
+  return (
+    <h2 className={styles.h2}>
+      <h1>dasdad</h1>Hellosdddsdsasdad Xiaoming
+    </h2>
+  )
 }
 
 export default App
